8d236f5fec708db6b965af3f42b5a2c6897d6e57
leshanshui-saagie <65134274+leshanshui-saagie@users.noreply.github.com>
2020-06-10 15:48:03 +0200

    Update dockerd-entrypoint.sh

--- dockerd-entrypoint.sh
+++ dockerd-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [[ "$1" = "serve" ]]; then
     shift 1
     torchserve --start --ts-config /home/model-server/config.properties --model-store /home/model-server/model-store
-    gunicorn -b 0.0.0.0:23333 serve-api
+    gunicorn -b 0.0.0.0:8079 serve-api
 else
     eval "$@"
 fi
